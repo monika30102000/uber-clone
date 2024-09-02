16660c18d3ae664fcc647c12f547b3304f75b3f4
monika30102000 <161137687+monika30102000@users.noreply.github.com>
2024-09-02 18:04:23 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "my_cluster" {
   role_arn = "arn:aws:iam::835370685428:role/ec2-admin"  # Replace with your IAM role ARN
 
   vpc_config {
-    subnet_ids         = ["subnet-04ab99c2c1c71a727", "subnet-0739caaeaf292a4cf"]  # Replace with your subnet IDs
+    subnet_ids         = ["subnet-02160107bcfbf413e", "subnet-064cc886dd33533ad"]  # Replace with your subnet IDs
     security_group_ids = ["sg-0bc1beb7788caf99a"]                         # Replace with your security group IDs
   }
 
@@ -16,7 +16,7 @@ resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = "arn:aws:iam::835370685428:role/ec2-admin"  # Replace with your IAM role ARN
-  subnet_ids         = ["subnet-04ab99c2c1c71a727", "subnet-0739caaeaf292a4cf"]      # Replace with your subnet IDs
+  subnet_ids         = ["subnet-02160107bcfbf413e", "subnet-064cc886dd33533ad"]      # Replace with your subnet IDs
 
   scaling_config {
     desired_size = 1
